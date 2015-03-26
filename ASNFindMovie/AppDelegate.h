3ca494b29ee2951e67dd05ddfb1e9375a416487b
YukiAsano <yuuki.1224.softtennis@gmail.com>
2015-03-26 10:01:50 -0700

    Initial Commit

---
+++ ASNFindMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASNFindMovie
+//
+//  Created by AsanoYuki on 2015/03/26.
+//  Copyright (c) 2015年 AsanoYuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASNFindMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASNFindMovie
+//
+//  Created by AsanoYuki on 2015/03/26.
+//  Copyright (c) 2015年 AsanoYuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
